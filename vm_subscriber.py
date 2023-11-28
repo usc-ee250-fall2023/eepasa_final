2fd3f61f08ef3039d43300184568322c1c85d002
kendal-win <77511524+kendal-win@users.noreply.github.com>
2023-11-28 01:39:20 -0800

    Update vm_subscriber.py

--- vm_subscriber.py
+++ vm_subscriber.py
@@ -58,9 +58,12 @@ def warning_callback(client, userdata, msg):
     #print(msg.payload.decode())
     #print(count)
 
-    if decrypt_msg == "Someone is coming!":
+    if (decrypt_msg == "Someone is coming!" and count == 0):
+        count += 1
         text(decrypt_msg)
-        time.sleep(10)
+    elif decrypt_msg == "Safe":
+        count = 0
+        
 """
     if decrypt_msg == "Someone is coming!":
         text(decrypt_msg)
