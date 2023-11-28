4d2659b0ca19805eda550eab21f85f9d0e5d6147
kendal-win <77511524+kendal-win@users.noreply.github.com>
2023-11-28 02:08:24 -0800

    Update vm_subscriber.py

--- vm_subscriber.py
+++ vm_subscriber.py
@@ -14,6 +14,7 @@ key = b'12345678909876543212345678909876'
 iv = b'1234567890987654'
 ######
 global count
+count = 0
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
