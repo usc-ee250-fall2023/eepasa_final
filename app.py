11097bac26a855aa29b0e05a0f369bcb2b9caae4
kendal-win <77511524+kendal-win@users.noreply.github.com>
2023-11-28 00:14:42 -0800

    try making it update

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def on_message(client, userdata, msg):
         sensor_data["distance"] = str(msg.payload, "utf-8")
     if msg.topic == "kackar/web_light":
         sensor_data["brightness"] = str(msg.payload, "utf-8")
+    index()
     
 sensor_data = {"distance": "N/A", "brightness": "N/A"}
 
